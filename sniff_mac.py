a089ada7739064d0dbfa36d33a382bba80570360
yuvallinux <yuvddd05@gmail.com>
2024-05-07 10:52:47 +0300

    sniff packet going to my computer

--- sniff_mac.py
+++ sniff_mac.py
@@ -1,7 +1,12 @@
-from scapy.all import sniff
+from scapy.all import sniff, Ether
+
+MY_MAC = '00:28:f8:6b:ea:cc'
+
+def filter_by_mac(frame):
+    return Ether in frame and frame[Ether].dst == MY_MAC
 
 def main():
-    frames = sniff(count = 2, iiface = 'wlp4s0')
+    frames = sniff(count = 1, iface = 'wlp4s0', lfilter = lambda f: Ether in f and f[Ether].dst == MY_MAC)
     first_frame = frames[0]
     first_frame.show()
 
